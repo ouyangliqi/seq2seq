910fad1c9b513439f2c1681558ad48f45f38ab65
ouyangliqi <ouyangliqi16@gmail.com>
2020-08-06 11:30:06 +0800

    version 0.1.5

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 nohup python ./seq2seq_tf2/bin/main.py \
+CUDA_VISIBLE_DEVICES=1 python ./seq2seq_tf2/bin/main.py \
                         --batch_size 64 \
                         --vocab_size 50000 \
                         --enc_units 256 \
                         --dec_units 256 \
                         --attn_units 256 \
-                        --mode 'test' > ./log/train.log &
+                        --mode 'train'
 
 
 CUDA_VISIBLE_DEVICES=0 python ./seq2seq_tf2/bin/main.py \
